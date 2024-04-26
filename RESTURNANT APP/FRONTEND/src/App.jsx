44deac80a97fb064ea310b1e44aff9eaafa20ca9
Mohd Anash khan <113244395+khananask908@users.noreply.github.com>
2024-04-26 22:22:20 +0530

    Add files via upload

---
+++ RESTURNANT APP/FRONTEND/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './Pages/Home';
+import NotFound from './Pages/NotFound';
+import Success from './Pages/Success';
+import './App.css'
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/success' element={<Success/>}/>
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+        <Toaster/>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ RESTURNANT APP/FRONTEND/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ANASH</div>
+          <div className="right">
+            <p>NEW DELHI ,SAKET , INDIA</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By ANASH KHAN </p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By mohdanashkhan.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
